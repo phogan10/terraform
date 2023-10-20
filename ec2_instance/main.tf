8cb2d31e785dbcafee54fdaa33b2819182a18f06
Philip Hogan <philiphogan10@gmail.com>
2023-10-19 21:17:43 -0400

    ec2 commit

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -5,7 +5,8 @@ provider "aws" {
 resource "aws_instance" "spacelift_demo_instance" {
   ami = "ami-0b5785917b1616d16"
   instance_type = "t2.micro"
-  subnet_id = ["subnet-0e146269065c4aec5"]
+  subnet_id = "subnet-0e146269065c4aec5"
+  security_groups = [ "sg-0a1dfe7fa79c68b2e" ]
 
   tags = {
     "Name" = "spacelift_demo_instance"
