910a27938bf52d978213201910dfb06d6b5f5725
Philip Hogan <philiphogan10@gmail.com>
2023-10-19 22:27:50 -0400

    changing ami

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "spacelift_demo_instance" {
-  ami = "ami-0b5785917b1616d16"
+  ami = "ami-0df435f331839b2d6"
   instance_type = "t2.micro"
   subnet_id = "subnet-0e146269065c4aec5"
   vpc_security_group_ids = [ "sg-0a1dfe7fa79c68b2e" ]
