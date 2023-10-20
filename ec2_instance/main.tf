5ec0e1ecb054e4906dac5604e3701bf5b6c23117
Philip Hogan <philiphogan10@gmail.com>
2023-10-19 21:25:20 -0400

    incorrect value type again

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -6,8 +6,7 @@ resource "aws_instance" "spacelift_demo_instance" {
   ami = "ami-0b5785917b1616d16"
   instance_type = "t2.micro"
   subnet_id = "subnet-0e146269065c4aec5"
-  security_groups = "sg-0a1dfe7fa79c68b2e"
-
+  vpc_security_group_ids = [ "sg-0a1dfe7fa79c68b2e" ]
   tags = {
     "Name" = "spacelift_demo_instance"
     "author" = "phogan"
